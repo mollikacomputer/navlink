ebc7a494a27ca8330bc81cce9f4715632070c4bd
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-09-14 19:13:33 +0600

    first commit

---
+++ src/app/blog/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogPage = () => {
+    return (
+        <div>
+            <h2 className='text-2xl' >Blog page</h2>
+        </div>
+    );
+};
+
+export default BlogPage;
